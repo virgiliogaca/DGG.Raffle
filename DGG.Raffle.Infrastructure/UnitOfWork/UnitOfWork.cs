61d87fcd1d536eff8c7519b70708f4f65faac84a
Virgilio Garcia <virgiliogaca@gmail.com>
2023-02-08 18:39:53 -0600

    Added unit of work and initialized the business service

---
+++ DGG.Raffle.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,74 @@
+﻿using DGG.Raffle.Infrastructure.Abstract.UnitOfWork;
+using DGG.Raffle.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace DGG.Raffle.Infrastructure.UnitOfWork
+{
+    /// <summary>
+    /// Unit of Work: Commits db changes by the services.
+    /// </summary>
+    /// <seealso cref="DGG.Raffle.Infrastructure.Abstract.UnitOfWork.IUnitOfWork" />
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool isDisposed;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EfUnitOfWork"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public UnitOfWork(DggRaffleDbContext context)
+        {
+            this.Context = context;
+        }
+
+        /// <summary>
+        /// Finalizes an instance of the <see cref="EfUnitOfWork"/> class.
+        /// </summary>
+        ~UnitOfWork()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Gets the context.
+        /// </summary>
+        /// <value>
+        /// The context.
+        /// </value>
+        public DbContext Context { get; }
+
+        /// <inheritdoc />
+        public async Task CompleteAsync()
+        {
+            await this.Context
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                Context.Dispose();
+            }
+
+            isDisposed = true;
+        }
+    }
+}
---
+++ DGG.Raffle.Service/Services/RaffleService.cs
@@ -0,0 +1,40 @@
+﻿using DGG.Raffle.Infrastructure.Abstract.Repositories;
+using DGG.Raffle.Infrastructure.Abstract.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGG.Raffle.Business.Services
+{
+    /// <summary>
+    /// Raffle business service, does operations to return to the controller.
+    /// </summary>
+    public class RaffleService
+    {
+        private readonly IRaffleEntriesRepository raffleEntriesRepository;
+        private readonly ICharitiesRepository _charitiesRepository;
+        private readonly IRaffleSessionsRepository _raffleSessionsRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RaffleService"/> class.
+        /// </summary>
+        /// <param name="raffleEntriesRepository">The raffle entries repository.</param>
+        /// <param name="charitiesRepository">The charities repository.</param>
+        /// <param name="raffleSessionsRepository">The raffle sessions repository.</param>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public RaffleService(
+            IRaffleEntriesRepository raffleEntriesRepository,
+            ICharitiesRepository charitiesRepository,
+            IRaffleSessionsRepository raffleSessionsRepository,
+            IUnitOfWork unitOfWork)
+        {
+            this.raffleEntriesRepository = raffleEntriesRepository;
+            _charitiesRepository = charitiesRepository;
+            _raffleSessionsRepository = raffleSessionsRepository;
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
